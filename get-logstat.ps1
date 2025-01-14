6a6e1f255310f54d73fa89943a80c8d5e830eed6
martinfridrich74 <martin.fridrich@outlook.cz>
2025-01-14 08:03:31 +0100

    get-logstat.ps1

--- get-logstat.ps1
+++ get-logstat.ps1
@@ -1,3 +1,45 @@
+<#
+    Copyright (c) 2025, Ing. Martin Fridrich
+    Všechna prava vyhrazena.
+    
+    Tento skript analyzuje logy systemu Windows a provadi ruzne statistiky na zaklade zadaneho casoveho obdobi.
+    Parametry logu, jako jsou nazy logu a casove obdobi, lze upravit dle potreby.
+    Autor: Ing. Martin Fridrich
+    Verze: 1.0
+    Datum: 2025-01-13
+
+    .SYNOPSIS
+    Tento skript analyzuje Windows logy na zaklade casoveho intervalu, vypocita prumerou velikost udalosti v bytech
+    a identifikuje nejvetsi 15minutovy casovy blok s nejvice udalostmi.
+
+    .DESCRIPTION
+    Skript umoznuje uzivateli zadat nazev logu (napriklad "Security", "System") a pocet dnu, pro ktere maji byt
+    udalosti analyzovany. Na zaklade techto parametru skript provede analyzu udalosti, vcetne vypoctu prumerne velikosti
+    udalosti a identifikace nejvetsiho casoveho intervalu s nejvice udalostmi.
+
+    .PARAMETER Days
+    Pocet dnu, ktere chcete zpetne analyzovat. Vychozi hodnota je 1.
+
+    .PARAMETER LogNames
+    Nazy logu k analyze (napr. "Security", "System"). Muzete zadat vice logu oddelene carkou.
+
+    .EXAMPLE
+    .\get-logstat.ps1 -Days 30 -LogNames 'Security', 'System'
+    Tento priklad provede analyzu logu "Security" a "System" za poslednich 30 dnu.
+
+    .EXAMPLE
+    .\get-logstat.ps1 -Days 7 -LogNames 'Application'
+    Tento priklad provede analyzu logu "Application" za poslednich 7 dnu.
+
+    .EXAMPLE
+    .\get-logstat.ps1 -Days 60 -LogNames 'Security'
+    Tento priklad provede analyzu logu "Security" za poslednich 60 dnu.
+
+    .NOTES
+    Autor: Ing. Martin Fridrich
+    Verze: 1.0
+    Datum: 2025-01-13
+#>
 param(
     # Pocatni pocet dnu pro analyzu (vychozi 1)
     [int]$Days = 1,
